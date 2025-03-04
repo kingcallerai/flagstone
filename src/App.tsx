4eb3915ec447a288d03eb45b374071cbe28a459b
kingcallerai <admin@kingcaller.ai>
2025-03-04 21:43:34 +0800

    app add

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <iframe
+        width="1920"
+        height="1080"
+        src="https://miro.com/app/embed/uXjVIXQc2nw=/?pres=1&frameId=3458764619273613184&embedId=779926906977"
+        allow="fullscreen; clipboard-read; clipboard-write"
+      ></iframe>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
