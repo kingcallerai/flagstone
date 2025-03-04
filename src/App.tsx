257dee6fb997b4eb2b856382ab2e43e6dec372c8
kingcallerai <admin@kingcaller.ai>
2025-03-05 02:31:43 +0800

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import "./App.css";
 function App() {
   return (
     <>
-      <iframe
-        width="1920"
-        height="1080"
-        src="https://miro.com/app/embed/uXjVIXQc2nw=/?pres=1&frameId=3458764619273613184&embedId=779926906977"
-        allow="fullscreen; clipboard-read; clipboard-write"
-      ></iframe>
+      <div width="max-width;">
+        <iframe
+          width="100%"
+          src="https://miro.com/app/embed/uXjVIXQc2nw=/?pres=1&frameId=3458764619273613184&embedId=779926906977"
+          allow="fullscreen; clipboard-read; clipboard-write"
+        ></iframe>
+      </div>
     </>
   );
 }
